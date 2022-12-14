eca141b69f1fb57f9467b49af9aa0a7cd74225b5
Satyasingh123 <99304733+Satyasingh123@users.noreply.github.com>
2022-12-14 17:04:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from os import getenv
 
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-MONGO_URL = getenv("MONGO_URL", "")
-BOT_ID = getenv("BOT_ID", "")#token first 10 digits
-AI_API_KEY = getenv("AI_API_KEY", "")
-AI_ID = int(getenv("AI_ID", ""))
+BOT_TOKEN = getenv("5845522997:AAF4sV9TJjPCBw4XQONQc4SHTOQ4DJRH1aI", "")
+MONGO_URL = getenv("mongodb+srv://Satya1234:1234satya@cluster0.1zgorbi.mongodb.net/?retryWrites=true&w=majority", "")
+BOT_ID = getenv("5825480776", "")#token first 10 digits
+AI_API_KEY = getenv("3b2150eab92f25f0e8d58e85a9f1227c", "")
+AI_ID = int(getenv("26011964", ""))
