530c4cf148366dac4cb421915aa3322c5213b851
Satyasingh123 <99304733+Satyasingh123@users.noreply.github.com>
2022-12-14 17:13:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ BOT_TOKEN = getenv("5845522997:AAF4sV9TJjPCBw4XQONQc4SHTOQ4DJRH1aI", "")
 MONGO_URL = getenv("mongodb+srv://Satya1234:1234satya@cluster0.1zgorbi.mongodb.net/?retryWrites=true&w=majority", "")
 BOT_ID = getenv("5825480776", "")#token first 10 digits
 AI_API_KEY = getenv("3b2150eab92f25f0e8d58e85a9f1227c", "")
-AI_ID = int(getenv("26011964", ""))
+AI_ID = int(getenv("AI_ID", "26011964"))
